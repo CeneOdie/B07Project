230e6938b9e13459b66721bc1b5ba37f7fdd440b
CeneOdie <ceneodendaal1@gmail.com>
2021-11-21 20:43:59 -0500

    login and signup with switching

--- app/src/main/java/com/example/b07project/ShowSignUpActivity.java
+++ app/src/main/java/com/example/b07project/ShowSignUpActivity.java
@@ -5,21 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class ShowSignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_show_sign_up);
     }
 
     public void showLogIn(View view) {
         Intent intent = new Intent(this, ShowLoginActivity.class);
         startActivity(intent);
     }
-    public void showSignUp(View view){
-        Intent intent = new Intent(this, ShowSignUpActivity.class);
-        startActivity(intent);
-    }
-
 }
