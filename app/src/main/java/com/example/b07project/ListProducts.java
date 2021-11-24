6413d29658d9414510d45fc99ff9a924bdfbab13
Cene <46448672+CeneOdie@users.noreply.github.com>
2021-11-24 13:37:53 -0500

    update og ListProducts

--- app/src/main/java/com/example/b07project/ListProducts.java
+++ app/src/main/java/com/example/b07project/ListProducts.java
@@ -1,12 +1,25 @@
-package com.example.b07project;
+package com.example.orderapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import android.os.Bundle;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ListProducts extends AppCompatActivity {
 
@@ -18,14 +31,59 @@ public class ListProducts extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Map<String, Object> items = new HashMap<>();
+        items.put("Brand", "Cows");
+        items.put("Name", "Milk");
+        items.put("Price", 5);
+        items.put("Store", null);
+
+        db.collection("Items").add(items)
+        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                System.out.println("Item added");
+            }
+        })
+        .addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                System.out.println("add failed");
+            }
+        });
+
+        db.collection("Items")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                System.out.println(document.getId() + " " + document.getData());
+                            }
+                        } else {
+                            System.out.println("There was an error");
+                        }
+                    }
+                });
+
+
+        /*
+        //db.collection("Items").getId()
         dbref = FirebaseDatabase.getInstance().getReference("Items");
         RecyclerView recyclerView;
-        recyclerView = findViewById(R.id.recycler_products);
+        recyclerView = (RecyclerView)findViewById(R.id.recycler_products);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         FirebaseRecyclerOptions<Product> options = new FirebaseRecyclerOptions.Builder<Product>().setQuery(dbref, Product.class).build();
         adapter = new ProductAdapter(options);
         recyclerView.setAdapter(adapter);
+
+        //implementation platform('com.google.firebase:firebase-bom:29.0.0');
+
+         */
+
     }
 
     @Override
