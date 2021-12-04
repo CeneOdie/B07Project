db12baaf1da4b1b4dac4a2ff5b329676b59c19b5
julia-yap <ya.park@mail.utoronto.ca>
2021-12-04 07:03:41 -0500

    Upload user story #2,3-related-layouts - Julia

---
+++ app/src/main/java/com/example/groceryapp/ItemRow.java
@@ -0,0 +1,58 @@
+package com.example.groceryapp;
+
+public class ItemRow {
+
+    String id, name;
+    double countItem, singlePrice, totPrice;
+
+    public ItemRow() {
+    }
+
+    public ItemRow(String id, String name, double countItem, double singlePrice, double totPrice) {
+        this.id = id;
+        this.name = name;
+        this.countItem = countItem;
+        this.singlePrice = singlePrice;
+        this.totPrice = totPrice;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getCountItem() {
+        return countItem;
+    }
+
+    public void setCountItem(double countItem) {
+        this.countItem = countItem;
+    }
+
+    public double getSinglePrice() {
+        return singlePrice;
+    }
+
+    public void setSinglePrice(double singlePrice) {
+        this.singlePrice = singlePrice;
+    }
+
+    public double getTotPrice() {
+        return totPrice;
+    }
+
+    public void setTotPrice(double totPrice) {
+        this.totPrice = totPrice;
+    }
+}
