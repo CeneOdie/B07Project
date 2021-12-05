7211cc2ee22c7d877515d9af11027958e262dbce
julia-yap <ya.park@mail.utoronto.ca>
2021-12-04 22:33:07 -0500

    upload ArchivedOrders activity

---
+++ app/src/main/java/com/example/groceryapp/Archived.java
@@ -0,0 +1,83 @@
+package com.example.groceryapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+
+public class Archived extends AppCompatActivity implements setRecyclerViewer {
+
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
+    AdapterOrder adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_archived);
+
+        // Hide default toolbar and its title
+        // Display custom toolbar
+        if (getSupportActionBar() != null)
+            getSupportActionBar().hide();
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        setSupportActionBar(findViewById(R.id.archivedTB));
+
+        // Return to listStoreOrders activity
+        FloatingActionButton btnBack = findViewById(R.id.backbtn);
+        btnBack.setOnClickListener(v -> startActivity(new Intent(Archived.this, listStoreOrders.class)));
+
+        setupRecyclerView();
+    }
+
+    @Override
+    public void setupRecyclerView() {
+
+        Query query = db.collection("Orders")
+                .orderBy("DateTime", Query.Direction.ASCENDING)
+                .whereEqualTo("Archived", true);
+
+        FirestoreRecyclerOptions<Order> options = new FirestoreRecyclerOptions.Builder<Order>()
+                .setQuery(query, Order.class).build();
+
+        // Interface here?
+        adapter = new AdapterOrder(options);
+        RecyclerView recyclerView = findViewById(R.id.archivedRecyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
+        // Swipe to delete order from activity and database
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                adapter.deleteOrder(viewHolder.getBindingAdapterPosition());
+            }
+        }).attachToRecyclerView(recyclerView);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+}
